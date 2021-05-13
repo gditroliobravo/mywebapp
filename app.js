ccf59eb5f4ce2c585185a4023a60540206620a04
Gerardo Di Trolio <gditroliobravo@users.noreply.github.com>
2021-05-13 11:52:16 -0400

    Whatever my message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var expressLayouts = require('express-ejs-layouts'); // around line 6
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,6 +12,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts); // around line 17
+
 
 app.use(logger('dev'));
 app.use(express.json());
